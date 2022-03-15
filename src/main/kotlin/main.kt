78414c0d5b971e91108097d6bdfb066a5174586e
aglyvin <alexej@glyvin.com>
2022-03-15 20:20:31 +0300

    Update main.kt
    11

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,6 @@
 fun main() {
     val likes = 21
 
-    val message = "Понравилось $likes " + if(likes%10 == 1) "человеку" else "людям"
+    val message = "Понравилось $likes " + if(likes%10 == 1 && likes%100 != 11) "человеку" else "людям"
     println(message)
 }
